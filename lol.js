e27cbec894cf73f66eb3eda23679df369914b589
Brian Ford <btford@umich.edu>
2014-06-06 11:01:45 -0700

    fix: patch Object.create
    Closes #2

--- lol.js
+++ lol.js
@@ -2,20 +2,30 @@
 module.exports = function patch () {
   var _defineProperty = Object.defineProperty;
   var _getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
+  var _create = Object.create;
 
   Object.defineProperty = function (obj, prop, desc) {
     if (isUnconfigurable(obj, prop)) {
       throw new TypeError('Cannot assign to read only property \'' + prop + '\' of ' + obj);
     }
-    return rewriteDescriptor(obj, prop, desc);
+    desc = rewriteDescriptor(obj, prop, desc);
+    return _defineProperty(obj, prop, desc);
   };
 
-  // delegate to defineProperty
   Object.defineProperties = function (obj, props) {
     Object.keys(props).forEach(function (prop) {
       Object.defineProperty(obj, prop, props[prop]);
     });
     return obj;
+  };
+
+  Object.create = function (obj, proto) {
+    if (typeof proto === 'object') {
+      Object.keys(proto).forEach(function (prop) {
+        proto[prop] = rewriteDescriptor(obj, prop, proto[prop]);
+      });
+    }
+    return _create(obj, proto);
   };
 
   Object.getOwnPropertyDescriptor = function (obj, prop) {
@@ -26,8 +36,9 @@ module.exports = function patch () {
     return desc;
   };
 
-  Object._redefineProperty = function (obj, prop, desc) {
-    return rewriteDescriptor(obj, prop, desc);
+  return function redefineProperty(obj, prop, desc) {
+    desc = rewriteDescriptor(obj, prop, desc);
+    return _defineProperty(obj, prop, desc);
   };
 };
 
@@ -36,14 +47,12 @@ function isUnconfigurable (obj, prop) {
 }
 
 function rewriteDescriptor (obj, prop, desc) {
+  desc.configurable = true;
   if (!desc.configurable) {
-    desc.configurable = true;
     if (!obj.__unconfigurables) {
-      _defineProperty(obj, '__unconfigurables', {
-        value: {}
-      });
+      _defineProperty(obj, '__unconfigurables', { writable: true, value: {} });
     }
     obj.__unconfigurables[prop] = true;
   }
-  return _defineProperty(obj, prop, desc);
+  return desc;
 }
