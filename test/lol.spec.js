154618add774049f022ab2407966fc4fd5031e34
Brian Ford <btford@umich.edu>
2014-06-06 12:02:59 -0700

    test: Object.create
    Closes #4

--- test/lol.spec.js
+++ test/lol.spec.js
@@ -46,4 +46,17 @@ describe('patch', function () {
     expect(object.y).toBe(5);
   });
 
+  it('should preserve the behavior of Object.create', function () {
+    var object = Object.create({}, {'y': { value: 5, writable: false }});
+    expect(object.y).toBe(5);
+
+    expect(function () {
+      Object.defineProperty(object, 'y', { value: 10 });
+    }).toThrow();
+    expect(object.y).toBe(5);
+
+    object.y = 10;
+    expect(object.y).toBe(5);
+  });
+
 });
